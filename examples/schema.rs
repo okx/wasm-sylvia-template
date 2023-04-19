11edfbce2620cf59f7055bb7a21baa508ddc6949
f <fan.ke@okg.com>
2023-04-19 18:41:57 +0800

    fix schema

--- examples/schema.rs
+++ examples/schema.rs
@@ -1,6 +1,9 @@
 use cosmwasm_schema::write_api;
+use {{crate_name}}::{ContractExecMsg, ContractQueryMsg, InstantiateMsg};
+use std::env::current_dir;
+use std::fs::create_dir_all;
+use cosmwasm_schema::{export_schema, remove_schemas, schema_for,write_api};
 
-use {{crate_name}}::contract::{ContractExecMsg, ContractQueryMsg, InstantiateMsg};
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
@@ -9,4 +12,14 @@ fn main() {
         execute: ContractExecMsg,
         query: ContractQueryMsg,
     }
+
+
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
+    create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
+
+    export_schema(&schema_for!(InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(ContractExecMsg), &out_dir);
+    export_schema(&schema_for!(ContractQueryMsg), &out_dir);
 }
